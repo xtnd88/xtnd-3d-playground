5794b591ef3b66086dd4a777ddedbeb5f05256bb
Andrea Moggridge <andi@xtnd.ai>
2025-12-23 14:42:23 +0000

    Placeholder 1930s house attempt (pre-GLB)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Grid } from "@react-three/drei";
+import House1930 from "./scene/House1930";
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [5, 4, 5], fov: 45 }}>
+      <Canvas camera={{ position: [12, 8, 12], fov: 45 }}>
         <ambientLight intensity={0.6} />
-        <directionalLight position={[5, 8, 5]} intensity={1} />
+        <directionalLight position={[10, 15, 10]} intensity={1} />
 
-        <Grid args={[20, 20]} />
+        <Grid args={[50, 50]} />
 
-        <mesh position={[0, 1, 0]}>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <House1930 />
 
         <OrbitControls makeDefault />
       </Canvas>
